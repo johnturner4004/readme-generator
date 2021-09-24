8f7787263dc56e4d7be3f31af2796ca26005ee56
johnturner4004 <johnturner4004@gmail.com>
2021-09-24 18:38:32 -0400

    move unused reducer files to archive folder

--- src/redux/reducer/_root.reducer.js
+++ src/redux/reducer/_root.reducer.js
@@ -1,23 +1,13 @@
 import { combineReducers } from 'redux';
-import conclusionCode from './conclusionCode.reducer';
-import gettingStartedCode from './gettingStartedCode.reducer';
 import licensesReducer from './licenses.reducer';
-import overviewCode from './overviewCode.reducer';
-import shieldCode from './shieldCode.reducer';
+import readme from './readme.reducer';
 import technologiesReducer from './technologies.reducer';
-import textInput from './textInput.reducer';
-import user from './user.reducer'
-import readme from './readme.reducer'
+import user from './user.reducer';
 
 const rootReducer = combineReducers({
-  // gettingStartedCode,
-  // overviewCode,
-  // shieldCode,
-  // textInput,
-  // conclusionCode,
-  technologiesReducer,
   licensesReducer,
   readme,
+  technologiesReducer,
   user,
 });
 
